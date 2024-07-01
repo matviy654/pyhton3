9d7f7d8f4ad2e1b64618b29b56f5da3adb58bdfc
matviy654 <matviy22.2008@gmail.com>
2024-07-01 20:23:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# input_string = input("Введіть рядок: ")
+# normalized_string = input_string.replace(" ", "").lower()
+# if normalized_string == normalized_string[::-1]:
+#     print("Рядок є паліндромом.")
+# else:
+#     print("Рядок не є паліндромом.")
+
+# text = input("Введіть текст: ")
+
+
+# reserved_words = input("Введіть список зарезервованих слів (розділених пробілами): ").split()
+
+
+# words = text.split()
+
+
+# modified_text = []
+# for word in words:
+#     if word in reserved_words:
+#         modified_text.append(word.upper())
+#     else:
+#         modified_text.append(word)
+
+
+# modified_text = ' '.join(modified_text)
+
+
+# print(modified_text)
